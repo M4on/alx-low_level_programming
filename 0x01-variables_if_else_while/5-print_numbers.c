f3ead899cef99ad86c0c0d73151fc61d5fd0d612
Emmanuel Akpan <mfonakpan@gmail.com>
2023-02-17 18:55:40 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Program to print single numbers of base 10,
+ * followed by  anewline.
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int a; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
