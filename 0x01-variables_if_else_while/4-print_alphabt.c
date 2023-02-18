a70a6404b6b590ba03caf63668cb9f85ebd3d8a0
Emmanuel Akpan <mfonakpan@gmail.com>
2023-02-17 18:33:53 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *  Description: Program to print a-z except 'e' and 'q'
+ *  Return; 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
