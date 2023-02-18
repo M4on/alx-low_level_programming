84b11a9973d527621621e05cac5171faf3e64418
Emmanuel Akpan <mfonakpan@gmail.com>
2023-02-18 04:41:58 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,19 +3,17 @@
 /**
  * main - Entry point
  * Description: Program to print single numbers of base 10,
- * followed by  anewline.
+ * followed by a newline.
  * Return: 0
  */
-
 int main(void)
 {
-	char c = '0';
-	
-	while (-1 < c <= '9')
-	{
-	       putchar(c);
-	       c++;
-	}
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
+
 	return (0);
 }
