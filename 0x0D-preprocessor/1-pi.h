e8292b9c5ab9b3db98d5adba5f9d731b6ce5aa2a
Emmanuel Akpan <mfonakpan@gmail.com>
2023-03-21 05:20:13 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: PI.h
+ * Auth: Emmanuel Akpan
+ * Desc: header file that defines a macro named
+ * PI as an abbreviation for the token 3.14159265359
+ */
+
+#define PI 3.14159265359
+#endif
