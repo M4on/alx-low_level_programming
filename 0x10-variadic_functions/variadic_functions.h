7d161bef484f5ac0df655cbfbdaca9965d2bfef3
Emmanuel Akpan <mfonakpan@gmail.com>
2023-03-24 01:43:53 -0700

    0-sum_them_all.c updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -27,4 +27,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endifi
+#endif
